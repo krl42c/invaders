a58065eb2a30b2443e4ea718b630d712ad5f0978
Karol <karolc9991@gmail.com>
2023-10-01 16:24:09 +0200

    build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-cl /MD /DEBUG /Z7 /std:c++17 /I C:\Users\Karol\dev\game\include src\main.cc src\game_scene.cc src\config.cc /LINK .\lib\raylib.lib user32.lib gdi32.lib opengl32.lib kernel32.lib winmm.lib shell32.lib
+cl /MD /DEBUG /Z7 /std:c++17 /I include src\main.cc src\game_scene.cc src\config.cc src\sprites.cc /LINK .\lib\raylib.lib user32.lib gdi32.lib opengl32.lib kernel32.lib winmm.lib shell32.lib
